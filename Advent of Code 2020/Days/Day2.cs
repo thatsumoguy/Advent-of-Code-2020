bc0f5752efe38c57e994b3142eb1a22ee99f7d55
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-02 11:14:19 -0500

    Small refactor

--- Advent of Code 2020/Days/Day2.cs
+++ Advent of Code 2020/Days/Day2.cs
@@ -10,13 +10,13 @@ namespace Advent_of_Code_2020.Days
         public static int PartOne(string[] input)
         {
             var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
-            return line.Where(x => x.pass.Count(i => i == x.validChar) >= x.min && x.pass.Count(j => j == x.validChar) <= x.max).Count();
+            return line.Where(x => x.Pass.Count(i => i == x.ValidChar) >= x.Min && x.Pass.Count(j => j == x.ValidChar) <= x.Max).Count();
         }
         public static int PartTwo(string[] input)
         {
             var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
-            return line.Where(x => x.pass.Count(i => i == x.validChar) >= x.min ^ x.pass.Count(j => j == x.validChar) <= x.max).Count();
+            return line.Where(x => x.Pass.Count(i => i == x.ValidChar) >= x.Min ^ x.Pass.Count(j => j == x.ValidChar) <= x.Max).Count();
         }
-        record Password(int min, int max, char validChar, string pass);
+        record Password(int Min, int Max, char ValidChar, string Pass);
     }
 }
