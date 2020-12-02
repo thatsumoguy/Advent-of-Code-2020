029abd884ccf7e0f6d031d616c46a556d5f8a2c4
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-02 09:57:58 -0500

    Re-wrote Day 2 with the new record type from C#9 and cleaned up the parsing

--- Advent of Code 2020/Days/Day2.cs
+++ Advent of Code 2020/Days/Day2.cs
@@ -9,38 +9,14 @@ namespace Advent_of_Code_2020.Days
     {
         public static int PartOne(string[] input)
         {
-            var output = 0;
-            foreach(var line in input)
-            {
-                var min = int.Parse(line.Substring(0, (line.IndexOf('-'))));
-                var max = int.Parse(line.Substring((line.IndexOf('-') + 1), (line.IndexOf(' ') - line.IndexOf('-'))));
-                var validChar = line[line.IndexOf(':') -1];
-                var password = line.Substring(line.IndexOf(':') + 2, (line.Length - line.IndexOf(':') - 2));
-                var count = password.Count(c => c == validChar);
-                if(count >= min && count <= max)
-                {
-                    output++;
-                }
-            }
-
-            return output;
+            var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
+            return line.Where(x => x.pass.Count(i => i == x.validChar) >= x.min && x.pass.Count(j => j == x.validChar) <= x.max).Count();
         }
         public static int PartTwo(string[] input)
         {
-            var output = 0;
-            foreach (var line in input)
-            {
-                var min = int.Parse(line.Substring(0, (line.IndexOf('-'))));
-                var max = int.Parse(line.Substring((line.IndexOf('-') + 1), (line.IndexOf(' ') - line.IndexOf('-'))));
-                var validChar = line[line.IndexOf(':') - 1];
-                var password = line.Substring(line.IndexOf(':') + 2, (line.Length - line.IndexOf(':') - 2));
-                if(password[min -1] == validChar ^ password[max -1] == validChar)
-                {
-                    output++;
-                }
-            }
-
-            return output;
+            var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
+            return line.Where(x => x.pass.Count(i => i == x.validChar) >= x.min ^ x.pass.Count(j => j == x.validChar) <= x.max).Count();
         }
+        record Password(int min, int max, char validChar, string pass);
     }
 }
