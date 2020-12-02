0eb5e26a8f3cc46b73285458c40577820d920cdd
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-02 09:26:06 -0500

    Day 2

--- Advent of Code 2020/Days/Day2.cs
+++ Advent of Code 2020/Days/Day2.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Advent_of_Code_2020.Days
 {
     class Day2
     {
+        public static int PartOne(string[] input)
+        {
+            var output = 0;
+            foreach(var line in input)
+            {
+                var min = int.Parse(line.Substring(0, (line.IndexOf('-'))));
+                var max = int.Parse(line.Substring((line.IndexOf('-') + 1), (line.IndexOf(' ') - line.IndexOf('-'))));
+                var validChar = line[line.IndexOf(':') -1];
+                var password = line.Substring(line.IndexOf(':') + 2, (line.Length - line.IndexOf(':') - 2));
+                var count = password.Count(c => c == validChar);
+                if(count >= min && count <= max)
+                {
+                    output++;
+                }
+            }
+
+            return output;
+        }
+        public static int PartTwo(string[] input)
+        {
+            var output = 0;
+            foreach (var line in input)
+            {
+                var min = int.Parse(line.Substring(0, (line.IndexOf('-'))));
+                var max = int.Parse(line.Substring((line.IndexOf('-') + 1), (line.IndexOf(' ') - line.IndexOf('-'))));
+                var validChar = line[line.IndexOf(':') - 1];
+                var password = line.Substring(line.IndexOf(':') + 2, (line.Length - line.IndexOf(':') - 2));
+                if(password[min -1] == validChar ^ password[max -1] == validChar)
+                {
+                    output++;
+                }
+            }
+
+            return output;
+        }
     }
 }
--- Advent of Code 2020/Program.cs
+++ Advent of Code 2020/Program.cs
@@ -8,9 +8,9 @@ namespace Advent_of_Code_2020
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines(@"C:\Users\Kyle Rinne\source\repos\Advent of Code 2020\Advent of Code 2020\Input\Day1.txt");
-            Console.WriteLine("Part One: " + Days.Day1.PartOne(input));
-            Console.WriteLine("Part Two: " + Days.Day1.PartTwo(input));
+            var input = File.ReadAllLines(@"C:\Users\Kyle Rinne\source\repos\Advent of Code 2020\Advent of Code 2020\Input\Day2.txt");
+            Console.WriteLine("Part One: " + Days.Day2.PartOne(input));
+            Console.WriteLine("Part Two: " + Days.Day2.PartTwo(input));
             Console.ReadLine();
         }
     }
