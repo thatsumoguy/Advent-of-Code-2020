2cd71299afd6a85d527a53a06988edf99b1b621d
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-03 08:22:00 -0500

    Added Day 3, changed Day 2 to a single liner

--- Advent of Code 2020/Days/Day2.cs
+++ Advent of Code 2020/Days/Day2.cs
@@ -9,13 +9,13 @@ namespace Advent_of_Code_2020.Days
     {
         public static int PartOne(string[] input)
         {
-            var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
-            return line.Where(x => x.Pass.Count(i => i == x.ValidChar) >= x.Min && x.Pass.Count(j => j == x.ValidChar) <= x.Max).Count();
+            return input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]))
+                .Where(x => x.Pass.Count(i => i == x.ValidChar) >= x.Min && x.Pass.Count(j => j == x.ValidChar) <= x.Max).Count(); ;
         }
         public static int PartTwo(string[] input)
         {
-            var line = input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]));
-            return line.Where(x => x.Pass.Count(i => i == x.ValidChar) >= x.Min ^ x.Pass.Count(j => j == x.ValidChar) <= x.Max).Count();
+            return input.Select(x => x.Split(new char[] { ' ', '-' })).Select(x => new Password(int.Parse(x[0]), int.Parse(x[1]), x[2][0], x[3]))
+                       .Where(x => x.Pass[x.Min -1] == x.ValidChar ^ x.Pass[x.Max -1] == x.ValidChar).Count();
         }
         record Password(int Min, int Max, char ValidChar, string Pass);
     }
---
+++ Advent of Code 2020/Days/Day3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2020.Days
+{
+    class Day3
+    {
+        public static long PartOne(string[] input)
+        {
+            var lines = input.Select(c => c.ToCharArray()).ToArray();
+            var width = input[0].Length;
+            return RunSlope(lines, width, 1, 3);
+        }
+
+        public static long PartTwo(string[] input)
+        {
+            var lines = input.Select(c => c.ToCharArray()).ToArray();
+            var width = input[0].Length;
+            return RunSlope(lines, width, 1, 3)*RunSlope(lines, width, 1, 1)*RunSlope(lines, width, 1, 5)*RunSlope(lines, width, 1, 7)*RunSlope(lines, width, 2, 1);
+        }
+
+        private static long RunSlope(char[][] lines, int width, int down, int right)
+        {
+            var trees = 0L;
+            var x = 0;
+            for (var y = 0; y < lines.Length; y += down)
+            {
+                if(lines[y][x] =='#')
+                {
+                    trees++;
+                }
+                x = (x + right) % width;
+            }
+            return trees;
+        }
+    }
+}
