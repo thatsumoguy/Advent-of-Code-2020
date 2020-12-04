865944f121d367bf288f4f9318f26cd9aedf724c
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-04 02:48:16 -0500

    Update Program.cs
    Removed name

--- Advent of Code 2020/Program.cs
+++ Advent of Code 2020/Program.cs
@@ -8,7 +8,7 @@ namespace Advent_of_Code_2020
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines(@"C:\Users\Kyle Rinne\source\repos\Advent of Code 2020\Advent of Code 2020\Input\Day4.txt");
+            var input = File.ReadAllLines(@"C:\Users\{name}\source\repos\Advent of Code 2020\Advent of Code 2020\Input\Day4.txt");
             Console.WriteLine("Part One: " + Days.Day4.PartOne(input));
             Console.WriteLine("Part Two: " + Days.Day4.PartTwo(input));
             Console.ReadLine();
