03c04c64a236e8dc5625ad3471f46f7034d8d1db
thatsumoguy <12224644+thatsumoguy@users.noreply.github.com>
2020-12-09 01:14:23 -0500

    Added Day9

---
+++ Advent of Code 2020/Days/Day9.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2020.Days
+{
+    class Day9
+    {
+        public static long PartOne(string[] input)
+        {
+            var xmas = input.Select(x => long.Parse(x)).ToArray();
+            foreach(var r in Enumerable.Range(25, input.Length))
+            {
+                var matched = false;
+                foreach(var i in Enumerable.Range(r - 25, r))
+                {
+                    foreach(var j  in Enumerable.Range(r - 25, r))
+                    {
+                        if(xmas[i] != xmas[j] && xmas[i] + xmas[j] == xmas[r])
+                        {
+                            matched = true;
+                        }
+                    }
+                }
+                if(!matched)
+                {
+                    return xmas[r];
+                }
+            }
+            return 0;
+        }
+
+        public static long PartTwo(string[] input)
+        {
+            var xmas = input.Select(x => long.Parse(x)).ToArray();
+            var invalidNum = 0l;
+            foreach (var r in Enumerable.Range(25, input.Length))
+            {
+                var matched = false;
+                foreach (var i in Enumerable.Range(r - 25, r))
+                {
+                    foreach (var j in Enumerable.Range(r - 25, r))
+                    {
+                        if (xmas[i] != xmas[j] && xmas[i] + xmas[j] == xmas[r])
+                        {
+                            matched = true;
+                        }
+                    }
+                }
+                if (!matched)
+                {
+                    invalidNum = xmas[r];
+                    break;
+                }
+            }
+            //var xmasSlice = new ArraySegment<long>(xmas);
+            foreach(var i in Enumerable.Range(0, xmas.Count()))
+            {
+                foreach(var j  in Enumerable.Range(i + 2, i))
+                {
+                    var count = xmas.Count();
+                    var current = xmas[i..j];
+                    var sum = current.Sum();
+                    if(sum == invalidNum)
+                    {
+                        return current.Min() + current.Max();
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
